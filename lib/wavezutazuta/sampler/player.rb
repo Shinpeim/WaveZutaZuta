7bc8496a093c52d2cd8693d5caee8e66b70fcfcf
moznion <moznion@gmail.com>
2013-04-17 03:41:18 +0900

    (信号処理の常識内での)複数のサンプリング周波数に対応

--- lib/wavezutazuta/sampler/player.rb
+++ lib/wavezutazuta/sampler/player.rb
@@ -6,6 +6,11 @@ module WaveZutaZuta
       include Sampler
 
       def initialize(*args)
+        unit_freq     = 24000 #[Hz]
+        samplerate    = args[0].samplerate
+        @sample_delta = samplerate.quo(unit_freq).round
+        raise "Sample rate of the data is too low. It must have minimum 24000Hz" if @sample_delta < 1
+
         device = CoreAudio.default_output_device
         @buffer = device.output_buffer(1024)
         @buffer.start
@@ -16,13 +21,16 @@ module WaveZutaZuta
       private
       def play_pcm(pcm_data)
         # device,pcmともに
-        # 量子化bit数が16bit,
-        # samplingrateが41100
+        # 量子化bit数が16bit
         # であることを前提としている
         # とりあえず手元で動くからいいけど動かないシステム,waveもあると思う
         pcm_samples = pcm_data.unpack("s*")
-        if @pcm_meta.channels == 2
-          pcm_samples = pcm_samples.each_slice(2).map{ |stereo| stereo.reduce(&:+) / 2 }
+
+        sample_delta = @sample_delta
+        sample_delta /= 2 if @pcm_meta.channels == 1
+
+        if sample_delta != 1
+          pcm_samples = pcm_samples.each_slice(sample_delta).map{ |stereo| stereo.reduce(&:+) / sample_delta }
         end
 
         wav = NArray.sint(pcm_samples.size)
